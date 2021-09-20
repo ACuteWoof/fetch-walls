cf59e0238e2d0157ebf2b0bced9a6be83668f9ac
ACuteWoof <sutharshanvithushan@gmail.com>
2021-09-20 15:35:36 +0530

    Bundle app to one file

--- main.py
+++ main.py
@@ -4,12 +4,57 @@ import gi
 import os
 import requests
 import random
+from requests import get
 
 from threading import Thread
-from fetcher import WallpaperFetcher
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GdkPixbuf, Gio
+from gi.repository import Gtk, Gio, GdkPixbuf
+
+class WallpaperFetcher:
+    def __init__(self, wallpaper_folder):
+        self.files = []
+        self.wallpaper_folder = wallpaper_folder.replace("~", os.getenv("HOME"))
+
+    def search(self, query, page):
+        query = query.replace(" ", "%20")
+        print(query)
+        url = f"https://wallhaven.cc/api/v1/search?q={query}"
+        print(url)
+        self.search_results = get(url).json()["data"][page]["path"]
+        return self.search_results
+
+    def filter_nsfw(self):
+        filtered_search_results
+        for i in self.search_results:
+            if i["purity"] == "sfw":
+                filtered_search_results.append(i)
+
+        self.search_results = filtered_search_results
+
+    def get_image(self, path):
+        data = self.search_results if data == None else data
+        pixbuf = GdkPixbuf.Pixbuf.new_from_at_scale(
+            path, width=220, height=220, preserve_aspect_ratio=True
+        )
+        btn = Gtk.EventBox()
+        btn.connect("button-press-event", self.get_preview_image, path)
+
+    def download_temp_image(self, path):
+        for file in os.listdir(self.wallpaper_folder):
+            os.system(f"rm {self.wallpaper_folder}/{file}")
+
+        response = get(path, stream=True)
+        file_name = path.split("/")[-1]
+        self.files.append(file_name)
+        self.file_location = f"{self.wallpaper_folder}/.temp.{file_name.split('.')[-1]}"
+        with open(self.file_location, "wb") as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                file.write(chunk)
+        return self.file_location
+
+    def copy_to_folder(self, folder, filename):
+        os.system(f"cp {self.file_location} {folder}/{filename}")
 
 envs = {
     "TEMP_WALLPAPER_FOLDER": os.getenv("TEMP_WALLPAPER_FOLDER"),
